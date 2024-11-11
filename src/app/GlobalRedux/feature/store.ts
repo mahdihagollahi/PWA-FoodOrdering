d2194e44fccf1a44f676d1b5f03c810c793584b4
mahdihaghollahi <mahdiaghollahi@gmail.com>
2024-11-12 00:23:51 +0330

    fix responsive

---
+++ src/app/GlobalRedux/feature/store.ts
@@ -0,0 +1,16 @@
+"use client"
+
+import { configureStore } from "@reduxjs/toolkit";
+import todoslice from './todoslice'
+
+ const store = configureStore({
+   reducer : {
+    todo : todoslice 
+   }
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store
+
+
+
